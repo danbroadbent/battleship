6ac70f8768969d8585a611bca5cf3df2fbfa5b9a
Daniel Broadbent <dbroadbent85@gmail.com>
2016-07-18 14:38:29 -0600

    added more parsing rules

--- lib/input_parse.rb
+++ lib/input_parse.rb
@@ -1,7 +1,19 @@
 class InputParse
 
   def parse_whitespace_case(input)
-    input.gsub(/\s+/, "").upcase
+      input.gsub(/\s+/, "").upcase
+    end
+
+  def valid_length?(input)
+    true if parse_whitespace_case(input).length == 4 || parse_whitespace_case(input).length == 6
+  end
+
+  def is_on_the_board?(input)
+    true if parse_whitespace_case(input).gsub(/[ABCDabcd1234]/, "") == ""
+  end
+
+  def is_ready_for_placement_parse?(input)
+    true if is_on_the_board?(input) && valid_length?(input)
   end
 
   def is_in_same_row_small?(input)
@@ -36,7 +48,7 @@ class InputParse
     true if is_ajacent_vertically_small?(input) == true && parse_whitespace_case(input)[0].ord == parse_whitespace_case(input)[4].ord + 2 || parse_whitespace_case(input)[0].ord == parse_whitespace_case(input)[4].ord - 2
   end
 
-  def is_valid?(input)
+  def is_valid_placement?(input)
     true if input.length == 4 && is_ajacent_vertically_small?(input) || is_ajacent_horizontally_small?(input) || input.length == 6 && is_ajacent_horizontally_medium?(input) || is_ajacent_vertically_medium?(input)
   end
 
--- test/input_parse_test.rb
+++ test/input_parse_test.rb
@@ -4,7 +4,7 @@ require './lib/input_parse'
 
 class InputParseTest < Minitest::Test
 
-  def test_it_can_remove_whitespace
+  def test_it_can_remove_whitespace_and_case_sensitivity
     user_input = InputParse.new
     assert_equal "A1A2" , user_input.parse_whitespace_case("A1 A2")
 
@@ -12,12 +12,29 @@ class InputParseTest < Minitest::Test
     assert_equal "A1A2A3" , user_input.parse_whitespace_case("A1 A2 A3")
   end
 
+  def test_it_is_the_right_length
+    user_input = InputParse.new
+    assert user_input.valid_length?("A1 a2")
+    assert user_input.valid_length?("a2 b3 c5")
+    refute user_input.valid_length?("a1")
+    refute user_input.valid_length?("")
+  end
+
   def test_placement_is_on_the_board
+    user_input = InputParse.new
+    assert_equal true , user_input.is_on_the_board?("a1")
+    refute user_input.is_on_the_board?("a6")
   end
 
   def test_it_is_case_insensitive
     user_input = InputParse.new
     assert_equal "A1A2" , user_input.parse_whitespace_case("a1 A2")
+  end
+
+  def test_is_ready_for_placement_parsing
+    user_input = InputParse.new
+    assert_equal true , user_input.is_ready_for_placement_parse?("a1b2")
+    refute user_input.is_ready_for_placement_parse?("a9")
   end
 
   def test_it_is_in_same_row
@@ -53,10 +70,10 @@ class InputParseTest < Minitest::Test
 
   def test_it_is_a_valid_coordinate
     user_input = InputParse.new
-    assert user_input.is_valid?("a1 a2")
-    assert user_input.is_valid?("a1 a2 A3")
+    assert user_input.is_valid_placement?("a1 a2")
+    assert user_input.is_valid_placement?("a1 a2 A3")
 
-    refute user_input.is_valid?("a1 a3 a4")
+    refute user_input.is_valid_placement?("a1 a3 a4")
   end
 
 end
