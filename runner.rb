dbdfd3dbbdb3ea254b94d29edb5888492d0a7d42
Daniel Broadbent <dbroadbent85@gmail.com>
2016-07-21 08:04:48 -0600

    fixed runner to keep program running after viewing instructions

--- runner.rb
+++ runner.rb
@@ -7,14 +7,17 @@ game = Game.new
 
 
   puts Messages.welcome_message
+  while true
     player_response = gets.chomp.upcase
     if player_response == "Q"
       puts "you quit..."
       exit
     elsif player_response == "I"
       puts Messages.instructions
+      redo
     elsif player_response == "P"
       game.start_small
     else
-      puts "Please enter a 'p', 'i', or 'q'"
+      puts Messages.welcome_message
     end
+end
